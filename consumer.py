7f8c208d7ac037e1ba6b63e0869187bba1a51833
cuong ba <ngoccuong0491@gmail.com>
2015-08-26 00:48:21 +0700

    add rabbit process

--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
 import users_pb2
 import sys
+import pika
+import config as cfg
 
 def show(list_users):
     for user in list_users.user:
@@ -8,15 +10,38 @@ def show(list_users):
         if user.HasField('email'):
             print("Email: {}".format(user.email))
 
+def receive_rabbit(list_users):
+    # Connect to RabbitMQ and create channel
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.RABBIT_HOST))
+    channel = connection.channel()
+
+    # Declare and listen queue
+    channel.queue_declare(queue=cfg.QUEUE_TOPIC)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    # Function process and print data
+    def callback(ch, method, properties, body):
+        #print("Method: {}".format(method))
+        #print("Properties: {}".format(properties))
+        list_users.ParseFromString(body)
+        show(list_users)
+    # Listen and receive data from queue
+    channel.basic_consume(callback, queue=cfg.QUEUE_TOPIC,no_ack=True)
+    channel.start_consuming()
+
 file_name = 'data'
 list_users = users_pb2.ListUsers()
 
 # Read existing list users
 try:
     f = open(file_name, 'rb')
-    list_users.ParseFromString(f.read())
+    #list_users.ParseFromString(f.read())
     f.close()
 
-    show(list_users)
+    #show(list_users)
+
+    # Receive Data from rabbit
+    receive_rabbit(list_users)
 except Exception as e:
     print("Read data is error")
--- producer.py
+++ producer.py
@@ -1,13 +1,26 @@
 import users_pb2
 import sys
+import pika
+import config as cfg
 
 def input(user):
-    user.id = 1#int(sys.raw_input("Enter user ID: "))
-    user.name = "cuongbn"#raw_input("Enter name: ")
-    email = "cuongbn@sigma-solutions.eu"#raw_input("Enter email address (blank for none): ")
-    
+    user.id = 1
+    user.name = "cuongbn"
+    email = "cuongbn@sigma-solutions.eu"
+
     if email != '':
         user.email = email
+
+def send_rabbit(data):
+    # Connect to RabbitMQ and create channel
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.RABBIT_HOST))
+    channel = connection.channel()
+    # Declare queue to send data
+    channel.queue_declare(queue=cfg.QUEUE_TOPIC)
+    # Send data
+    channel.basic_publish(exchange='', routing_key=cfg.QUEUE_TOPIC, body=data)
+    print(" [x] Sent data to RabbitMQ")
+    connection.close()
 
 
 file_name = 'data'
@@ -26,9 +39,13 @@ input(list_users.user.add())
 
 # Write the new list users to file
 try:
-    f = open(file_name, 'wb')
+    #f = open(file_name, 'wb')
     data_encode = list_users.SerializeToString()
-    f.write(data_encode)
-    f.close()
+    # Send to rabbit
+    send_rabbit(data_encode)
+
+    #f.write(data_encode)
+    #f.close()
 except Exception as e:
     print("Write file is error")
+    print(e)
